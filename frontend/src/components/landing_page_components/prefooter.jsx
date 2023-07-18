21bf0c3f96d26a4cd372390b17a377495bf2ec6e
Chinonso Wisdom <nonsowisdom62@gmail.com>
2023-07-18 20:11:20 +0100

    Responsive landing page with bootstrap

--- frontend/src/components/landing_page_components/prefooter.jsx
+++ frontend/src/components/landing_page_components/prefooter.jsx
@@ -4,9 +4,9 @@ import '../../css/index.css';
 
 const PreFooter = () => {
     return (
-        <div className='mt-5 text-center mild-dark bgg-padding-y text-white'>
-                    <h2>Hire remote developers</h2>
-                    <h3>Tell us the skills you need and we will find the best developer for you</h3>
+        <div className=' mt-3 mt-lg-5 text-center mild-dark bgg-padding-y text-white'>
+                    <h1 className='bd-600 mb-4'>Hire remote developers</h1>
+                    <h3 className='bd-300 mb-4'>Tell us the skills you need and we will find the best developer for you</h3>
                     <button className="btn btn-lg btn-primary">Hire developers</button>
         </div>
     )
